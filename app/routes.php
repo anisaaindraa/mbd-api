dde8d1595c0071feaf672580bde3af5d89e072da
anisaaindraa <11211014@student.itk.ac.id>
2023-10-23 19:49:31 +0800

    first commit

--- app/routes.php
+++ app/routes.php
@@ -10,12 +10,11 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-
     // get
-    $app->get('/countries', function (Request $request, Response $response) {
+    $app->get('/ReadBuah', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->query('SELECT * FROM countries');
+        $query = $db->query('SELECT * FROM ReadBuah');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
 
@@ -23,94 +22,15 @@ return function (App $app) {
     });
 
     // get by id
-    $app->get('/countries/{id}', function (Request $request, Response $response, $args) {
+    $app->get('/ReadBuah/{id_buah}', function (Request $request, Response $response, $args) {
         $db = $this->get(PDO::class);
 
-        $query = $db->prepare('SELECT * FROM countries WHERE id=?');
-        $query->execute([$args['id']]);
+        $query = $db->prepare('SELECT * FROM ReadBuah WHERE id_buah=?');
+        $query->execute([$args['id_buah']]);
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results[0]));
 
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // post data
-    $app->post('/countries', function (Request $request, Response $response) {
-        $parsedBody = $request->getParsedBody();
-
-        $id = $parsedBody["id"]; // menambah dengan kolom baru
-        $countryName = $parsedBody["name"];
-
-        $db = $this->get(PDO::class);
-
-        $query = $db->prepare('INSERT INTO countries (id, name) values (?, ?)');
-
-        // urutan harus sesuai dengan values
-        $query->execute([$id, $countryName]);
-
-        $lastId = $db->lastInsertId();
-
-        $response->getBody()->write(json_encode(
-            [
-                'message' => 'country disimpan dengan id ' . $lastId
-            ]
-        ));
-
-        return $response->withHeader("Content-Type", "application/json");
-    });
-
-    // put data
-    $app->put('/countries/{id}', function (Request $request, Response $response, $args) {
-        $parsedBody = $request->getParsedBody();
-
-        $currentId = $args['id'];
-        $countryName = $parsedBody["name"];
-        $db = $this->get(PDO::class);
-
-        $query = $db->prepare('UPDATE countries SET name = ? WHERE id = ?');
-        $query->execute([$countryName, $currentId]);
-
-        $response->getBody()->write(json_encode(
-            [
-                'message' => 'country dengan id ' . $currentId . ' telah diupdate dengan nama ' . $countryName
-            ]
-        ));
-
-        return $response->withHeader("Content-Type", "application/json");
-    });
-
-    // delete data
-    $app->delete('/countries/{id}', function (Request $request, Response $response, $args) {
-        $currentId = $args['id'];
-        $db = $this->get(PDO::class);
-
-        try {
-            $query = $db->prepare('DELETE FROM countries WHERE id = ?');
-            $query->execute([$currentId]);
-
-            if ($query->rowCount() === 0) {
-                $response = $response->withStatus(404);
-                $response->getBody()->write(json_encode(
-                    [
-                        'message' => 'Data tidak ditemukan'
-                    ]
-                ));
-            } else {
-                $response->getBody()->write(json_encode(
-                    [
-                        'message' => 'country dengan id ' . $currentId . ' dihapus dari database'
-                    ]
-                ));
-            }
-        } catch (PDOException $e) {
-            $response = $response->withStatus(500);
-            $response->getBody()->write(json_encode(
-                [
-                    'message' => 'Database error ' . $e->getMessage()
-                ]
-            ));
-        }
-
-        return $response->withHeader("Content-Type", "application/json");
-    });
 };
--- app/settings.php
+++ app/settings.php
@@ -23,7 +23,7 @@ return function (ContainerBuilder $containerBuilder) {
                 ],
                 'db' => [
                     'host' => '127.0.0.1',
-                    'dbname' => 'mbd-slim-api',
+                    'dbname' => 'toko_buah',
                     'user' => 'root',
                     'password' => '',
                 ]
